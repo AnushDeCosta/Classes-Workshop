0dac37a3c8e267e9310cc227ca6b6843c3c63eff
Anush De Costa <anush.de.costa@gmail.com>
2025-04-24 21:29:59 +1000

    Added cat.py file
    # Added Cat class
    
    1. Created a new Python file named cat.py.
    2. Defined the Cat class with attributes: name, nickname, cry, and locations.
    3. Initialised locations as an empty list for exploration tracking.

---
+++ animals/cat.py
@@ -0,0 +1,10 @@
+class Cat():
+    """
+    This is the class Cat that implements a cat with 3 attributes (name, nickname, cry and locations)
+    """
+    def __init__(self):
+        self.name = "Pat"
+        self.nickname = "FatPat"
+        self.cry = "Meow"
+        self.locations = []
+
